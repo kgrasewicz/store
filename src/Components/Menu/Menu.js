9168b6c7fa57a6deb56ff92c16e8c5340a096480
Katarzyna Grasewicz <grasewiczkatarzyna@gmail.com>
2021-05-11 12:27:03 +0200

    fix menu links

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -3,10 +3,10 @@ import React from "react";
 const menu = () => {
   return (
     <ul className="main-menu">
-      <li><a href="https://kgrasewicz.github.io/jewellery-store/shop/all" className="menu-font">// shop</a></li>
-      <li><a href="https://kgrasewicz.github.io/jewellery-store/about" className="menu-font">// about us</a></li>
-      <li><a href="https://kgrasewicz.github.io/jewellery-store/terms" className="menu-font">// terms & shipment</a></li>
-      <li><a href="https://kgrasewicz.github.io/jewellery-store/contact" className="menu-font">// contact us</a></li>
+      <li><a href="/shop/all" className="menu-font">// shop</a></li>
+      <li><a href="/about" className="menu-font">// about us</a></li>
+      <li><a href="/terms" className="menu-font">// terms & shipment</a></li>
+      <li><a href="/contact" className="menu-font">// contact us</a></li>
     </ul>
 
   );
