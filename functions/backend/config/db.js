e1752ea75ffe6d2b5376a9132891c39f398592f2
Katarzyna Grasewicz <grasewiczkatarzyna@gmail.com>
2021-05-13 22:31:32 +0200

    deploy netlify project

--- functions/backend/config/db.js
+++ functions/backend/config/db.js
@@ -6,7 +6,7 @@ const connectDB = async () => {
     try {
         //database Name
         const databaseName='Store';
-        const con = await mongoose.connect(`mongodb+srv://admin:Catherine1998admin%21@cluster0.lasnf.mongodb.net/Store?authSource=admin&replicaSet=atlas-vaq5td-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, { 
+        const con = await mongoose.connect(`mongodb+srv://admin:F7PPcTHJigsobFGT@cluster0.lasnf.mongodb.net/Store?authSource=admin&replicaSet=atlas-vaq5td-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true`, { 
         useNewUrlParser: true,
         useUnifiedTopology: true,
         useCreateIndex: true
